5ec83df157befaa74e810285e2723e57535aa6cc
Riyas Valiyadan <riyas.vmc@gmail.com>
2023-06-05 15:10:35 +0530

    Initial commit

---
+++ ConsolePrintingWindowsExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var document = new PrintDocument();
+        document.PrintPage += PrintPageHandler;
+
+        // Set printer name (optional)
+        //document.PrinterSettings.PrinterName = "Printer Name";
+
+        // Set other print settings if needed (e.g., page orientation, paper size, etc.)
+        //document.DefaultPageSettings.Landscape = true;
+        //document.DefaultPageSettings.PaperSize = new PaperSize("Custom", 300, 400);
+
+        // Start printing
+        document.Print();
+    }
+
+    static void PrintPageHandler(object sender, PrintPageEventArgs e)
+    {
+        // Get the Graphics object to draw on the page
+        Graphics graphics = e.Graphics;
+
+        // Specify the font, brush, and coordinates for printing
+        var font = new Font("Arial", 12);
+        Brush brush = Brushes.Black;
+        float x = 10;
+        float y = 10;
+
+        // Print the text
+        graphics.DrawString("Hello, world!", font, brush, x, y);
+    }
+}
